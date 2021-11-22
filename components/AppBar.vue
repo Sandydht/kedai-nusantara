8a258775e4ccc174d053d9f99761e4f9628fa7d9
Sandydht <sandydwi75@gmail.com>
2021-11-22 14:57:35 +0700

    feat: Create an AppBar component

---
+++ components/AppBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="appBar">
+    <button @click="click">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+      </svg>
+    </button>
+    <h1>Kedai Nusantara</h1>
+    <nav-bar />
+  </div>
+</template>
+
+<script>
+import NavBar from '~/components/NavBar.vue'
+
+export default {
+  name: 'AppBar',
+  components: {
+    NavBar
+  },
+  emits: ['handleClick'],
+  methods: {
+    click () {
+      this.$emit('handleClick')
+    }
+  }
+}
+</script>

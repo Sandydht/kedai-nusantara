27cc5d9b3a26be2ed9dd1e731e03afbe745f2b4d
Sandydht <sandydwi75@gmail.com>
2021-11-22 15:00:02 +0700

    feat: Create an SideBar component

---
+++ components/SideBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="sideBar" @click="click">
+    <nav-bar />
+  </div>
+</template>
+
+<script>
+import NavBar from '~/components/NavBar.vue'
+
+export default {
+  name: 'SideBar',
+  components: {
+    NavBar
+  },
+  emits: ['handleClick'],
+  methods: {
+    click () {
+      this.$emit('handleClick')
+    }
+  }
+}
+</script>

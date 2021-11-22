ca37c44b2e8ed9831c0b300068a74de908bc67ce
Sandydht <sandydwi75@gmail.com>
2021-11-22 15:05:19 +0700

    feat: Toggle sidebar

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,32 @@
 <template>
-  <Tutorial />
+  <div id="app">
+    <app-bar @handleClick="toggleSidebar" />
+    <div v-if="showSidebar">
+      <side-bar @handleClick="toggleSidebar" />
+    </div>
+    <nuxt />
+  </div>
 </template>
 
 <script>
-export default {}
+import AppBar from '~/components/AppBar.vue'
+import SideBar from '~/components/SideBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppBar,
+    SideBar
+  },
+  data () {
+    return {
+      showSidebar: false
+    }
+  },
+  methods: {
+    toggleSidebar () {
+      this.showSidebar = !this.showSidebar
+    }
+  }
+}
 </script>

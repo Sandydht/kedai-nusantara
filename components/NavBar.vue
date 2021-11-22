a7f25a31405974e19041ef5bb7e1058d9f2ea3a5
Sandydht <sandydwi75@gmail.com>
2021-11-22 15:01:07 +0700

    feat: Create an NavBar component

---
+++ components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="navBar">
+    <nuxt-link to="/">
+      Home
+    </nuxt-link>
+    <nuxt-link to="/favorite">
+      Favorite
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
